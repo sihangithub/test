2c040a79bd688d81b05148dcc47e0c20a4c2a857
sihangithub <74972188+sihangithub@users.noreply.github.com>
2022-10-23 16:53:48 +0800

    Add files via upload

--- script.js
+++ script.js
@@ -2,9 +2,9 @@ var num = 0
 function newLine(){
     num++;
     if (num%2 == 0)
-        document.getElementById("title").innerHTML = "New Line";
+        document.getElementById("title").innerHTML = "YX\u2665 (click me)";
     else
-        document.getElementById("title").innerHTML = "lol";
+        document.getElementById("title").innerHTML = "YX (click me)";
     
 }
 
